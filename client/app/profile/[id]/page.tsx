3cf92a689b96aa4847eb7785c7929256f554ca8f
yuki0911 <s2110052@u.tsukuba.ac.jp>
2025-03-17 19:16:12 +0900

    インポートパスの修正とコンポーネントエラーの改善

--- client/app/profile/[id]/page.tsx
+++ client/app/profile/[id]/page.tsx
@@ -1,15 +1,11 @@
-import { Suspense } from 'react';
-import ProfileContent from './ProfileContent';
+import ProfileClient from './ProfileClient';
 
-type Props = {
-  params: { id: string };
-  searchParams: { [key: string]: string | string[] | undefined };
-};
+interface PageProps {
+  params: {
+    id: string;
+  };
+}
 
-export default function ProfilePage({ params }: Props) {
-  return (
-    <Suspense fallback={<div>読み込み中...</div>}>
-      <ProfileContent id={params.id} />
-    </Suspense>
-  );
+export default function Page({ params }: PageProps) {
+  return <ProfileClient id={params.id} />;
 } 
